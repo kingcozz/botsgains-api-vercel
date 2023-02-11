6dce480fbbf50916ab40941bfcfd596e0d278156
kingcozz <92553627+kingcozz@users.noreply.github.com>
2023-02-11 11:45:32 -0500

    init

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    DEV_API: 'http://localhost:3000',
+    PRODUCTION_API: 'https://bot-gains-ui.vercel.app',
+  },
+};
+
+export default nextConfig;
