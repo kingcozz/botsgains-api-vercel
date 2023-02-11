b2dd1a38c6375d0ef237bd0c6fe9c90965127b5f
kingcozz <kingcozz88@gmail.com>
2023-02-11 11:48:55 -0500

    init

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+// config
+import { HOST_API } from '../config';
+
+// ----------------------------------------------------------------------
+
+export default function IndexPage() {
+  console.log('HOST_API', HOST_API);
+
+  return <h1>TEST</h1>;
+}
